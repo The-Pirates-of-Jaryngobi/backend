d334a8fc10433c6baed660279e4e1ec57a46ba05
Jaeho-99 <wogh6860@gmail.com>
2024-03-03 14:35:10 +0900

    feat: main 로직 1차 완료

--- aws_lambda/lambda_function.py
+++ aws_lambda/lambda_function.py
@@ -50,7 +50,7 @@ def get_ingredient_info_list(cursor, recipe_id: int) -> list:
 	"""
 	return ingredient_info_list
 
-# "product" 테이블에서 해당 재료명에 대해서 정보를 반환하는 함수.
+# "product" 테이블에서 해당 재료명에 대해서 상품 정보를 반환하는 함수.
 def get_product_info(cursor, ingredient_name: str) -> tuple:
 	product_price = None
 	product_unit_price = None
@@ -61,7 +61,7 @@ def get_product_info(cursor, ingredient_name: str) -> tuple:
 	return (product_price, product_unit_price, product_url)
 
 # 총 가격을 계산하는 함수.
-def calculate_total_price(ingredient_info_list: list, product_info_list: list) -> float:
+def get_total_price(ingredient_infos: list) -> float:
 	total_price = 0.0
 	"""
 	로직 작성.
@@ -100,6 +100,7 @@ def lambda_handler(event, context):
     menu_name = preprocess_menu_name(menu_name)
     menu_id = get_menu_id(cursor=cursor, menu_name=menu_name)
     if menu_id is None:
+        conn.close()
         return {
             'statusCode': 404,
             'body': json.dumps('메뉴명이 존재하지 않습니다.')
@@ -107,6 +108,7 @@ def lambda_handler(event, context):
 		
     recipe_id_list = get_recipe_id_list(cursor=cursor, menu_id=menu_id) # 레시피 목록 검색
     if len(recipe_id_list) == 0:
+        conn.close()
         return {
             'statusCode': 404,
             'body': json.dumps('메뉴명에 대한 레시피가 존재하지 않습니다.')
@@ -115,21 +117,65 @@ def lambda_handler(event, context):
     recipe_infos = {
 			'recipe_id_list' : [],
 			'ingredient_info_list' : [],
-			'product_info_list' : [],
 			'recipe_total_price_list' : []
 	}
     # 각 레시피에 대한 가격 정보 조회
     for recipe_id in recipe_id_list:
         recipe_infos['recipe_id_list'].append(recipe_id)
         ingredient_info_list = get_ingredient_info_list(cursor=cursor, recipe_id=recipe_id)
-        recipe_infos['ingredient_info_list'].append(ingredient_info_list)
+        ingredient_infos = []
         # 각 재료에 대한 정보 조회
         for ingredient_info in ingredient_info_list:
             ingredient_name = ingredient_info[0]
             ingredient_volume = ingredient_info[1]
             ingredient_unit = ingredient_info[2]
-            """
-            위와 같은 방식으로 코드 작성 예정...
-            """
+            product_price, product_unit_price, product_url = get_product_info(cursor=cursor, ingredient_name=ingredient_name)
+            ingredient_infos.append([ingredient_name, product_unit_price, product_price, ingredient_volume, ingredient_unit, product_url])
+        
+        recipe_total_price = get_total_price(ingredient_infos=ingredient_infos) # 레시피 총 가격 계산
+        recipe_infos['ingredient_info_list'].append(ingredient_infos)
+        recipe_infos['recipe_total_price_list'].append(recipe_total_price)
+    
+    # recipe_total_price_list에서 최저값의 인덱스를 찾은 후 해당 레시피 아이디로 유튜브 데이터 조회.
+    min_price_index = recipe_infos['recipe_total_price_list'].index(min(recipe_infos['recipe_total_price_list']))
+    min_recipe_id = recipe_infos['recipe_id_list'][min_price_index]
+    youtube_url, youtube_thumbnail, youtube_title, channel_name, channel_img = get_youtube_info(cursor=cursor, recipe_id=min_recipe_id)
+    
+    # 최저가 레시피의 데이터 추출.
+    total_price = recipe_infos['recipe_total_price_list'][min_price_index]
+    ingredient_list = []
+    ingredient_info_list = recipe_infos['ingredient_info_list'][min_price_index]
+    for i in range(len(ingredient_info_list)):
+        ingredient_name = ingredient_info_list[i][0]
+        ingredient_unit_price = ingredient_info_list[i][1]
+        ingredient_price = ingredient_info_list[i][2]
+        ingredient_volume = ingredient_info_list[i][3]
+        ingredient_unit = ingredient_info_list[i][4]
+        ingredient_url = ingredient_info_list[i][5]
+        
+        ingredient = {
+            "ingredient_name": ingredient_name,
+            "ingredient_unit_price": ingredient_unit_price,
+            "ingredeint_price": ingredient_price,
+            "ingredient_volume" : ingredient_volume,
+            "ingredient_unit" : ingredient_unit,
+            "ingredient_url" : ingredient_url
+        }
+        ingredient_list.append(ingredient)
+    
+    result = {
+        "youtube_url": youtube_url,
+        "youtube_thumbnail": youtube_thumbnail,
+        "youtube_title": youtube_title,
+        "channel_name": channel_name,
+        "channel_img": channel_img,
+        "total_price": total_price,
+        "ingredient_list": ingredient_list
+    }
     
     conn.close()
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(result)
+    }
