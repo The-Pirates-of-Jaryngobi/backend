0b2a3a4e53b384d1bd0889e556b375428343ed45
Jaeho-99 <wogh6860@gmail.com>
2024-03-07 15:00:47 +0900

    fix: 일부 로직 수정 3/7(목) 회의 내용 기반

--- jaringobi_be/jaringobi/lambda_function.py
+++ jaringobi_be/jaringobi/lambda_function.py
@@ -12,7 +12,7 @@ def connect_to_postgres():
     rds_host = "de-6-1-test.ch4xfyi6stod.ap-northeast-2.rds.amazonaws.com"
     username = "jaryngobi"
     password = "pirates-recipe"
-    db_name = "service"
+    db_name = "test"
 
     try:
         conn = psycopg2.connect(host=rds_host, user=username, password=password, dbname=db_name)
@@ -25,10 +25,8 @@ def connect_to_postgres():
 # 웹에서 검색한 메뉴명 전처리 함수
 # ex) "소 불고기" => "소불고기"
 def preprocess_menu_name(menu_name: str) -> str:
-    logging.info("menu name: {}".format(menu_name))
+    print("menu name: {}".format(menu_name))
     return menu_name.replace(" ", "")
-
-
 
 # "menu" 테이블에서 메뉴의 이름의 id를 반환하는 함수
 def get_menu_id(cursor, menu_name: str) -> int:
@@ -48,7 +46,6 @@ def get_menu_id(cursor, menu_name: str) -> int:
         menu_id = result[0]
 
     return menu_id
-
 
 # "recipe" 테이블에서 해당 메뉴 아이디를 갖는 레시피 리스트를 반환하는 함수
 def get_recipe_id_list(cursor, menu_id: str) -> list:
@@ -71,7 +68,6 @@ def get_recipe_id_list(cursor, menu_id: str) -> list:
     # 만약 레시피 목록이 없다면 빈 리스트 반환할 예정
     return recipe_id_list
 
-
 # "ingredient" 테이블에서 해당 레시피 아이디를 갖는 재료 정보를 반환하는 함수
 def get_ingredient_info_list(cursor, recipe_id: str) -> list:
     ingredient_info_list = []
@@ -87,55 +83,54 @@ def get_ingredient_info_list(cursor, recipe_id: str) -> list:
     """
     cursor.execute(sql_query, (recipe_id,))
     ingredient_infos = cursor.fetchall()
-
+    
     for ingredient in ingredient_infos:
         ingredient_info_list.append(list(ingredient))
 
     return ingredient_info_list
 
-
 # "product" 테이블에서 해당 재료명에 대해서 최저가 상품 정보를 반환하는 함수.
 def get_cheapest_product_info(cursor, ingredient_name: str) -> tuple:
     product_price = None
-    product_unit_price = None
+    product_gram_price = None
     product_url = None
 
-    # 해당 재료명에 대한 쿼리. unit_price가 가장 싼 것 위주로 검색. 만약 없다면 그냥 price를 기준으로.
+    # 해당 재료명에 대한 쿼리. gram_price가 가장 싼 것 위주로 검색. 만약 없다면 그냥 price를 기준으로.
     sql_query = """
-    SELECT price, unit_price, url 
+    SELECT price, gram_price, url 
     FROM (
-        SELECT price, unit_price, url, ingredient_name
+        SELECT price, gram_price, url, ingredient_name
         FROM product_1
         UNION ALL
-        SELECT price, unit_price, url, ingredient_name
+        SELECT price, gram_price, url, ingredient_name
         FROM product_2
         UNION ALL
-        SELECT price, unit_price, url, ingredient_name
+        SELECT price, gram_price, url, ingredient_name
         FROM product_3
         UNION ALL
-        SELECT price, unit_price, url, ingredient_name
+        SELECT price, gram_price, url, ingredient_name
         FROM product_4
         UNION ALL
-        SELECT price, unit_price, url, ingredient_name
+        SELECT price, gram_price, url, ingredient_name
         FROM product_5
         UNION ALL
-        SELECT price, unit_price, url, ingredient_name
+        SELECT price, gram_price, url, ingredient_name
         FROM product_6
         UNION ALL
-        SELECT price, unit_price, url, ingredient_name
+        SELECT price, gram_price, url, ingredient_name
         FROM product_7
         UNION ALL
-        SELECT price, unit_price, url, ingredient_name
+        SELECT price, gram_price, url, ingredient_name
         FROM product_8
         UNION ALL
-        SELECT price, unit_price, url, ingredient_name
+        SELECT price, gram_price, url, ingredient_name
         FROM product_9
         UNION ALL
-        SELECT price, unit_price, url, ingredient_name
+        SELECT price, gram_price, url, ingredient_name
         FROM product_10
     ) AS combined_products
     WHERE ingredient_name = %s
-    ORDER BY COALESCE(unit_price, price) ASC 
+    ORDER BY COALESCE(gram_price, price) ASC 
     LIMIT 1
     """
     cursor.execute(sql_query, (ingredient_name,))
@@ -144,30 +139,29 @@ def get_cheapest_product_info(cursor, ingredient_name: str) -> tuple:
     if product_info:
         # 결과에서 필요한 정보 추출
         product_price = product_info[0]  # 가격
-        product_unit_price = product_info[1]  # 단위당 가격
+        product_gram_price = product_info[1]  # 단위당 가격
         product_url = product_info[2]  # 상품 링크 정보
 
-    return (product_price, product_unit_price, product_url)
+    return (product_price, product_gram_price, product_url)
 
 # 총 가격을 계산하는 함수.
 def get_total_price(ingredient_infos: list) -> float:
     total_price = 0.0
+    ingredient_list_without_unit = []
     for i in range(len(ingredient_infos)):
-        # ingredient_name = ingredient_infos[i][0] # 재료명
+        ingredient_name = ingredient_infos[i][0] # 재료명
         product_unit_price = ingredient_infos[i][1] # 단위당 가격
-        product_price = ingredient_infos[i][2] # 가격
+        # product_price = ingredient_infos[i][2] # 가격
         ingredient_volume = ingredient_infos[i][3] # 첨가량
         # ingredient_unit = ingredient_infos[i][4] # 첨가 단위
         # product_url = ingredient_infos[i][5] # 상품 링크
 
         if product_unit_price and ingredient_volume: # 만약 단위당 가격이 존재한다면
             total_price += ingredient_volume * product_unit_price # ex) 10(g) * 1g당355원 -> 3550
-        elif product_price: # 가격만 존재한다면
-            total_price += product_price
-        else: # 단위당 가격, 가격 둘 다 존재하지 않는 경우. (예외 상황)
-            continue
+        else:
+            ingredient_list_without_unit.append(ingredient_name)
 
-    return total_price
+    return total_price, ingredient_list_without_unit
 
 # "youtube_video", "channel" 테이블에서 해당 레시피 아이디에 대해서 정보를 반환하는 함수.
 def get_youtube_info(cursor, recipe_id: str) -> tuple:
@@ -200,13 +194,9 @@ def get_youtube_info(cursor, recipe_id: str) -> tuple:
 
     return (youtube_url, youtube_thumbnail, youtube_title, channel_name, channel_img)
 
-
 def lambda_handler(menu_name):
     conn = connect_to_postgres()
     cursor = conn.cursor()
-
-    # body = json.loads(event['body'])
-    # menu_name = body.get('menu_name')
 
     menu_name = preprocess_menu_name(menu_name)
     menu_id = get_menu_id(cursor=cursor, menu_name=menu_name)
@@ -228,7 +218,8 @@ def lambda_handler(menu_name):
     recipe_infos = {
         'recipe_id_list' : [],
         'ingredient_info_list' : [],
-        'recipe_total_price_list' : []
+        'recipe_total_price_list' : [],
+        'ingredient_list_without_unit' : []
     }
 
     # 각 레시피에 대한 가격 정보 조회
@@ -244,12 +235,13 @@ def lambda_handler(menu_name):
             product_price, product_unit_price, product_url = get_cheapest_product_info(cursor=cursor, ingredient_name=ingredient_name)
             ingredient_infos.append([ingredient_name, product_unit_price, product_price, ingredient_volume, ingredient_unit, product_url])
 
-        recipe_total_price = get_total_price(ingredient_infos=ingredient_infos) # 레시피 총 가격 계산
+        recipe_total_price, ingredients_without_unit = get_total_price(ingredient_infos=ingredient_infos) # 레시피 총 가격 계산
         recipe_infos['ingredient_info_list'].append(ingredient_infos)
         recipe_infos['recipe_total_price_list'].append(recipe_total_price)
+        recipe_infos['ingredient_list_without_unit'].append(ingredients_without_unit)
 
-    # recipe_total_price_list에서 최저가의 인덱스를 찾은 후 해당 레시피 아이디로 유튜브 데이터 조회.
-    valid_indices = [i for i, price in enumerate(recipe_infos['recipe_total_price_list']) if price != 0]
+    # recipe_total_price_list에서 최저가의 인덱스를 찾은 후 해당 레시피 아이디로 유튜브 데이터 조회. 조건: 가격이 존재 and 레시피 재료가 3개 이상.
+    valid_indices = [i for i, price in enumerate(recipe_infos['recipe_total_price_list']) if price != 0 and len(recipe_infos['ingredient_info_list'][i]) >= 3]
     min_price_index = min(valid_indices, key=lambda x: recipe_infos['recipe_total_price_list'][x])
     min_recipe_id = recipe_infos['recipe_id_list'][min_price_index]
     
@@ -257,6 +249,7 @@ def lambda_handler(menu_name):
     
     # 최저가 레시피의 데이터 추출.
     total_price = recipe_infos['recipe_total_price_list'][min_price_index]
+    ingredient_list_without_unit = recipe_infos['ingredient_list_without_unit'][min_price_index]
     ingredient_list = []
     ingredient_info_list = recipe_infos['ingredient_info_list'][min_price_index]
     for i in range(len(ingredient_info_list)):
@@ -284,7 +277,8 @@ def lambda_handler(menu_name):
         "channel_name": channel_name,
         "channel_img": channel_img,
         "total_price": total_price,
-        "ingredient_list": ingredient_list
+        "ingredient_list": ingredient_list,
+        "ingredient_list_without_unit": ingredient_list_without_unit
     }
 
     conn.close()
